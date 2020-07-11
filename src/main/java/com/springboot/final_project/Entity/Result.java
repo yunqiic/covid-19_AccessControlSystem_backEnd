d57731482b9875d3e1438e36a9a03f71b5af6d57
491369719@qq.com <a491369719>
2020-07-11 15:24:11 +0800

    第一次整合

---
+++ src/main/java/com/springboot/final_project/Entity/Result.java
@@ -0,0 +1,14 @@
+package com.springboot.final_project.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Result {
+    private Integer result;
+}
--- src/main/java/com/springboot/final_project/controller/adminController.java
+++ src/main/java/com/springboot/final_project/controller/adminController.java
@@ -2,13 +2,13 @@ package com.springboot.final_project.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.springboot.final_project.Entity.Admin;
+import com.springboot.final_project.Entity.Result;
 import com.springboot.final_project.mapper.AdminMapper;
 import com.springboot.final_project.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 import java.util.Objects;
 
 @RestController
@@ -20,19 +20,21 @@ public class adminController {
 
     @Autowired
     AdminMapper adminMapper;
-
+//(@RequestParam("username") String username,@RequestParam("password") String password,@RequestParam("comfirm_password") String comfirm_password)
     @PostMapping("/admin/create")
-    public int creatAdmin(String username,String password,String comfirm_password){
+    public Result creatAdmin(@RequestBody Map<String, String> map){
 
-        Integer result = 3;
-        if(!Objects.equals(password, comfirm_password)){
-            result =2;
+        Result result = new Result();
+        result.setResult(3);
+        if(!Objects.equals(map.get("password"), map.get("comfirm_password"))){
+            result.setResult(2);
             return result;
         }else {
             Admin admin = new Admin();
-            admin.setUsername(username);
-            admin.setPassword(password);
-            result=adminService.createAdmin(admin);
+            admin.setUsername(map.get("username"));
+            admin.setPassword(map.get("password"));
+            System.out.println(admin.toString());
+            result.setResult(adminService.createAdmin(admin));;
             return result;
         }
 
