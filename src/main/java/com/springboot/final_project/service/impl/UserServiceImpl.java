a0abce8286261e966ec4ed289d5f767df8b99cef
陈继伟 <447846781@qq.com>
2020-07-12 15:57:00 +0800

    后台后端住户部分

---
+++ src/main/java/com/springboot/final_project/service/impl/UserServiceImpl.java
@@ -0,0 +1,198 @@
+package com.springboot.final_project.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.springboot.final_project.Entity.User;
+import com.springboot.final_project.mapper.UserMapper;
+import com.springboot.final_project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+
+    @Override
+    public int add(User user){
+        return userMapper.insert(user);
+    }
+
+    @Override//按筛选条件获取住户列表
+    public Map<String,Object> getList(User user, int index, int limit, String sort) {
+        Page<User> page = new Page<>(index,limit);
+        //1只查id
+        if(user.getId()!= null && user.getName().equals("") && user.getIs_locked()==null && user.getHealth_status()==null) {
+            userMapper.selectPage(page, new QueryWrapper<User>().like("id", user.getId()));
+        }
+        //2只查name
+        else if(user.getId() == null && !user.getName().equals("") &&user.getIs_locked()==null&&user.getHealth_status()==null)
+            userMapper.selectPage(page,new QueryWrapper<User>().like("name",user.getName()));
+        //3只查锁定
+        else if(user.getId() == null && user.getName().equals("") && user.getIs_locked()!=null && user.getHealth_status()==null){
+            if(user.getIs_locked()==true)userMapper.selectPage(page,new QueryWrapper<User>().eq("is_locked",1));
+            else userMapper.selectPage(page,new QueryWrapper<User>().eq("is_locked",0));
+        }
+        //4只查健康
+        else if(user.getId() == null && user.getName().equals("") && user.getIs_locked()==null && user.getHealth_status()!=null){
+            //healthy
+            if(user.getHealth_status()==0)userMapper.selectPage(page,new QueryWrapper<User>().lt("health_status",3));//小于3
+            else userMapper.selectPage(page,new QueryWrapper<User>().ge("health_status",3));//大于等于3
+        }
+        //5查id和name
+        else if(user.getId()!= null && !user.getName().equals("") && user.getIs_locked()==null && user.getHealth_status()==null){
+            userMapper.selectPage(page, new QueryWrapper<User>().like("id", user.getId()).like("name",user.getName()));
+        }
+        //6查id和锁定
+        else if(user.getId()!= null && user.getName().equals("") && user.getIs_locked()!=null && user.getHealth_status()==null){
+            if(user.getIs_locked()==true)userMapper.selectPage(page,new QueryWrapper<User>().eq("is_locked",1).like("id", user.getId()));
+            else userMapper.selectPage(page,new QueryWrapper<User>().eq("is_locked",0).like("id", user.getId()));
+        }
+        //7查id和健康
+        else if(user.getId() != null && user.getName().equals("") && user.getIs_locked()==null && user.getHealth_status()!=null){
+            //healthy
+            if(user.getHealth_status()==0)userMapper.selectPage(page,new QueryWrapper<User>().lt("health_status",3).like("id", user.getId()));//小于3
+            else userMapper.selectPage(page,new QueryWrapper<User>().ge("health_status",3).like("id", user.getId()));//大于等于3
+        }
+        //8查name和锁定
+        else if(user.getId()== null && !user.getName().equals("") && user.getIs_locked()!=null && user.getHealth_status()==null){
+            if(user.getIs_locked()==true)userMapper.selectPage(page,new QueryWrapper<User>().eq("is_locked",1).like("name",user.getName()));
+            else userMapper.selectPage(page,new QueryWrapper<User>().eq("is_locked",0).like("name",user.getName()));
+        }
+        //9name和健康
+        else if(user.getId() == null && !user.getName().equals("") && user.getIs_locked()==null && user.getHealth_status()!=null){
+            //healthy
+            if(user.getHealth_status()==0)userMapper.selectPage(page,new QueryWrapper<User>().lt("health_status",3).like("name",user.getName()));//小于3
+            else userMapper.selectPage(page,new QueryWrapper<User>().ge("health_status",3).like("name",user.getName()));//大于等于3
+        }
+        //10锁定和健康
+        else if(user.getId() == null && user.getName().equals("") && user.getIs_locked()!=null && user.getHealth_status()!=null){
+            if(user.getIs_locked()==true){
+                if(user.getHealth_status()==0){
+                    userMapper.selectPage(page,new QueryWrapper<User>().lt("health_status",3).eq("is_locked",1));//小于3
+                }
+                else {
+                    userMapper.selectPage(page,new QueryWrapper<User>().ge("health_status",3).eq("is_locked",1));//大于等于3
+                }
+            }
+            else {
+                if(user.getHealth_status()==0){
+                    userMapper.selectPage(page,new QueryWrapper<User>().lt("health_status",3).eq("is_locked",0));//小于3
+                }
+                else {
+                    userMapper.selectPage(page,new QueryWrapper<User>().ge("health_status",3).eq("is_locked",0));//大于等于3
+                }
+            }
+        }
+        //11查id、name、锁定
+        else if(user.getId()!= null && !user.getName().equals("") && user.getIs_locked()!=null && user.getHealth_status()==null){
+            if(user.getIs_locked()==true)userMapper.selectPage(page,new QueryWrapper<User>().eq("is_locked",1).like("id", user.getId()).like("name",user.getName()));
+            else userMapper.selectPage(page,new QueryWrapper<User>().eq("is_locked",0).like("id", user.getId()).like("name",user.getName()));
+        }
+        //12查id、name、健康
+        else if(user.getId() != null && !user.getName().equals("") && user.getIs_locked()==null && user.getHealth_status()!=null){
+            //healthy
+            if(user.getHealth_status()==0)userMapper.selectPage(page,new QueryWrapper<User>().lt("health_status",3).like("id", user.getId()).like("name",user.getName()));//小于3
+            else userMapper.selectPage(page,new QueryWrapper<User>().ge("health_status",3).like("id", user.getId()).like("name",user.getName()));//大于等于3
+        }
+        //13查name、锁定、健康
+        else if(user.getId() == null && !user.getName().equals("") && user.getIs_locked()!=null && user.getHealth_status()!=null){
+            if(user.getIs_locked()==true){
+                if(user.getHealth_status()==0){
+                    userMapper.selectPage(page,new QueryWrapper<User>().lt("health_status",3).eq("is_locked",1).like("name",user.getName()));//小于3
+                }
+                else {
+                    userMapper.selectPage(page,new QueryWrapper<User>().ge("health_status",3).eq("is_locked",1).like("name",user.getName()));//大于等于3
+                }
+            }
+            else {
+                if(user.getHealth_status()==0){
+                    userMapper.selectPage(page,new QueryWrapper<User>().lt("health_status",3).eq("is_locked",0).like("name",user.getName()));//小于3
+                }
+                else {
+                    userMapper.selectPage(page,new QueryWrapper<User>().ge("health_status",3).eq("is_locked",0).like("name",user.getName()));//大于等于3
+                }
+            }
+        }
+        //14查id、锁定、健康
+        else if(user.getId() != null && user.getName().equals("") && user.getIs_locked()!=null && user.getHealth_status()!=null){
+            if(user.getIs_locked()==true){
+                if(user.getHealth_status()==0){
+                    userMapper.selectPage(page,new QueryWrapper<User>().lt("health_status",3).eq("is_locked",1).like("id", user.getId()));//小于3
+                }
+                else {
+                    userMapper.selectPage(page,new QueryWrapper<User>().ge("health_status",3).eq("is_locked",1).like("id", user.getId()));//大于等于3
+                }
+            }
+            else {
+                if(user.getHealth_status()==0){
+                    userMapper.selectPage(page,new QueryWrapper<User>().lt("health_status",3).eq("is_locked",0).like("id", user.getId()));//小于3
+                }
+                else {
+                    userMapper.selectPage(page,new QueryWrapper<User>().ge("health_status",3).eq("is_locked",0).like("id", user.getId()));//大于等于3
+                }
+            }
+        }
+        //15全都查
+        else if(user.getId() != null && !user.getName().equals("") && user.getIs_locked()!=null && user.getHealth_status()!=null){
+            if(user.getIs_locked()==true){
+                if(user.getHealth_status()==0){
+                    userMapper.selectPage(page,new QueryWrapper<User>().lt("health_status",3).eq("is_locked",1).like("id", user.getId()).like("name",user.getName()));//小于3
+                }
+                else {
+                    userMapper.selectPage(page,new QueryWrapper<User>().ge("health_status",3).eq("is_locked",1).like("id", user.getId()).like("name",user.getName()));//大于等于3
+                }
+            }
+            else {
+                if(user.getHealth_status()==0){
+                    userMapper.selectPage(page,new QueryWrapper<User>().lt("health_status",3).eq("is_locked",0).like("id", user.getId()).like("name",user.getName()));//小于3
+                }
+                else {
+                    userMapper.selectPage(page,new QueryWrapper<User>().ge("health_status",3).eq("is_locked",0).like("id", user.getId()).like("name",user.getName()));//大于等于3
+                }
+            }
+        }
+        //16全都为null
+        else userMapper.selectPage(page,null);
+
+        List<User> users = page.getRecords();
+        if(sort.equals("-id")) Collections.reverse(users);
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("list",users);
+        map.put("total",page.getTotal());
+        return  map;
+    }
+
+    @Override
+    public void delete(int id) {
+        userMapper.deleteById(id);
+    }
+
+    @Override
+    public void lock(int id){
+        User user = userMapper.selectById(id);
+        user.setIs_locked(true);
+        userMapper.updateById(user);
+    }
+
+    @Override
+    public void unLock(int id){
+        User user = userMapper.selectById(id);
+        user.setIs_locked(false);
+        userMapper.updateById(user);
+    }
+
+    @Override
+    public User getById(int id){
+        User user = userMapper.selectById(id);
+        return user;
+    }
+
+}
